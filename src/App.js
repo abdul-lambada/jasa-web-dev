a04e3b0f23832030a31ed506a38a61848c64483e
Abdul Kholik <akl859349@gmail.com>
2025-06-09 16:33:22 +0700

    remove debug

--- src/App.js
+++ src/App.js
@@ -28,24 +28,6 @@ const FrameworkIcon = () => (
 );
 
 // Phone icon removed to fix ESLint warning
-
-const MailIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail">
-    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-  </svg>
-);
-
-const SparklesIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-sparkles">
-    <path d="M9.9 10.95a2.5 2.5 0 0 0 3.1 3.1L22 22l-8.05-8.05a2.5 2.5 0 0 0-3.1-3.1L2 2l8.05 8.05Z"></path>
-    <path d="M14.5 5.5L16 7"></path>
-    <path d="M8.5 8.5L7 10"></path>
-    <path d="M14.5 18.5L16 17"></path>
-    <path d="M8.5 15.5L7 14"></path>
-  </svg>
-);
-
 const App = () => {
   // State untuk Testimoni
   const [testimonialName, setTestimonialName] = useState('');
